fffb179d002bb8c62e6f32f01193b5febd5aa908
achmadar <60347050+achmadar@users.noreply.github.com>
2020-04-09 17:37:04 +0700

    Add files via upload
    Project Pertama

---
+++ SQL Server/API/objects/tbl_user.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * 
+ */
+class User
+{
+	private $conn;
+	private $table_name = "tbl_user";
+
+	public $id_user;
+	public $name;
+	public $nohp;
+	public $point;
+	public $username;
+	public $password;
+
+	public function __construct($db){
+		$this->conn = $db;
+	}
+
+	function read(){
+		$query = " SELECT * FROM " . $this->table_name;
+
+		$stmt = $this->conn->prepare($query);
+
+		$stmt->execute();
+		return $stmt;
+	}
+
+	function create()
+	{
+		$query = "INSERT INTO " . $this -> table_name. " SET name=:name, point=:point, nohp=:nohp,username=:username, password=:password";
+
+		$stmt = $this->conn->prepare($query);
+
+		$this->name=htmlspecialchars(strip_tags($this->name));
+		$this->point=htmlspecialchars(strip_tags($this->point));
+		$this->nohp=htmlspecialchars(strip_tags($this->nohp));
+		$this->username=htmlspecialchars(strip_tags($this->username));
+		$this->password=htmlspecialchars(strip_tags($this->password));
+
+		$stmt->bindParam(":name",$this->name);
+		$stmt->bindParam(":point",$this->point);
+		$stmt->bindParam(":nohp",$this->nohp);
+		$stmt->bindParam(":username",$this->username);
+		$stmt->bindParam(":password",$this->password);
+
+		if ($stmt->execute()) {
+			return true;
+		}
+		return false;
+
+	}
+
+	function update()
+	{
+		$query = "UPDATE " . $this -> table_name . " SET name=:name, point=:point, nohp=:nohp,username=:username, password=:password WHERE id_user=:id_user";
+		$stmt = $this->conn->prepare($query);
+
+		$this->name=htmlspecialchars(strip_tags($this->name));
+		$this->point=htmlspecialchars(strip_tags($this->point));
+		$this->nohp=htmlspecialchars(strip_tags($this->nohp));
+		$this->username=htmlspecialchars(strip_tags($this->username));
+		$this->password=htmlspecialchars(strip_tags($this->password));
+
+		$stmt->bindParam(":name",$this->name);
+		$stmt->bindParam(":point",$this->point);
+		$stmt->bindParam(":nohp",$this->nohp);
+		$stmt->bindParam(":username",$this->username);
+		$stmt->bindParam(":password",$this->password);
+
+		if ($stmt->execute()) {
+			return true;
+		}
+		return false;
+
+
+
+	}
+	function delete()
+	{
+		$query = "DELETE FROM " . $this -> table_name . " WHERE id_user =?";
+		$stmt = $this->conn->prepare($query);
+
+		$this->password=htmlspecialchars(strip_tags($this->id_user));
+
+		$stmt->bindParam(1,$this->id_user);
+
+		if ($stmt->execute()) {
+			return true;
+		}
+		return false;
+	}
+
+}
+?>
