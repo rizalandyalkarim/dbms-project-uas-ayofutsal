fe600fbd636a1284a7dbb22dcbc3d575833d367f
daffaraka <49283525+daffaraka@users.noreply.github.com>
2020-04-21 15:28:40 +0700

    Add files via upload

---
+++ aplikasi_booking_tempat_futsal-master/app/src/main/java/com/example/aplikasi_sewa_tempat_futsal/BookingLapangan.kt
@@ -0,0 +1,12 @@
+package com.example.aplikasi_sewa_tempat_futsal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BookingLapangan : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_booking_lapangan)
+    }
+}
---
+++ aplikasi_booking_tempat_futsal-master/app/src/main/java/com/example/aplikasi_sewa_tempat_futsal/CaraPesan.kt
@@ -0,0 +1,19 @@
+package com.example.aplikasi_sewa_tempat_futsal
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class CaraPesan : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cara_pesan)
+        MainActivity()
+
+    }
+
+
+
+}
